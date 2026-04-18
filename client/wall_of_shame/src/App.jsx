f3a1aef30143f374cc5c0785adbac0dd9844aa45
Gouransh <gouranshgupta01@gmail.com>
2026-04-18 23:07:24 +0530

    feat: added the navigation bar in the app.jsx and made a separate wallpage.jsx for the home page

--- client/wall_of_shame/src/App.jsx
+++ client/wall_of_shame/src/App.jsx
@@ -1,32 +1,25 @@
-import { useEffect, useState } from "react";
-//useState allows to remember data state and allows to automatically re-renders the frontend if there is any change in the data
-//useEffect - this helps in fetching data from a server
-
-import "./app.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import WallPage from "./pages/wallpage";
+// import ProfilePage from "./ProfilePage"; // You will create this later
 
 function App() {
-  const [snippets, setSnippets] = useState([]); //here snippets is my dataset  which is initially empty whereas setSnippets is the function to set update the state of the snippets
-
-  useEffect(() => {
-    fetch("http://localhost:5000/snippets")
-      .then(res => res.json())
-      .then(data => setSnippets(data));
-  }, []); //we are first fetching the data by going to that URL and then converting it into a json object and then finally storing that data in snippets
-
-  //now we are converting the snippets into html template and returning the html
   return (
-    <div>
-      <h1>Wall of Shame </h1>
-      {snippets.map(s => (
-          <div key={s.id} className="card">
-            <div className="confession">
-              {s.confession}
-            </div>
-
-            <pre className="code">{s.code}</pre>
+    <BrowserRouter>
+      <nav className="navbar">
+        <div className="nav-logo"> Wall of Shame</div>
+        <div className="nav-links">
+          <Link to="/" className="nav-item">Global Wall</Link>
+          <Link to="/profile" className="nav-item">Profile</Link>
         </div>
-      ))}
-    </div>
+      </nav>
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<WallPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/wall_of_shame/src/pages/wallpage.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+//useState allows to remember data state and allows to automatically re-renders the frontend if there is any change in the data
+//useEffect - this helps in fetching data from a server
+
+import "../app.css";
+
+function wallpage() {
+  const [snippets, setSnippets] = useState([]); //here snippets is my dataset  which is initially empty whereas setSnippets is the function to set update the state of the snippets
+
+  useEffect(() => {
+    fetch("http://localhost:5000/snippets")
+      .then(res => res.json())
+      .then(data => setSnippets(data));
+  }, []); //we are first fetching the data by going to that URL and then converting it into a json object and then finally storing that data in snippets
+
+  //now we are converting the snippets into html template and returning the html
+  return (
+    <div>
+      <h1>Wall of Shame </h1>
+      {snippets.map(s => (
+          <div key={s.id} className="card">
+            <div className="confession">
+              {s.confession}
+            </div>
+
+            <pre className="code">{s.code}</pre>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default wallpage;
