deddf2c2e67bdd6b25f8f83bdcc4b53f81d92e7a
Gouransh <gouranshgupta01@gmail.com>
2026-04-18 08:37:06 +0530

    feat: added the feature to handle comment requests

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const PORT = 5000;
 const path = require("path");
 const fs = require("fs"); //using node built in file manager required for reading and writing the database.json file
 
+// express.json() parses incoming JSON request bodies so we can read req.body that will be used in adding the new snippets in the database
+app.use(express.json());
 
 function getDataDb(){
     const data = fs.readFileSync("db.json"); //reading the data from the db.json file in a synchronous form as the server waits until the read is complete
@@ -22,7 +24,6 @@ app.get("/snippets",(req,res)=>{
     const data = getDataDb();
     res.json(data.snippets);
 });
-
 app.post("/snippets",(req,res)=>{
     const newSnippet = {          //creating a new snippet
         id: Date.now(), 
@@ -35,6 +36,28 @@ app.post("/snippets",(req,res)=>{
     writeDataDb(data);
     res.json(newSnippet);
 });
+
+app.post("/snippets/:id/comments", (req,res)=>{
+    const data = getDataDb();
+    const snippet = data.snippets.find(             //finding the snippet of that particular id for which we are willing to add comment
+        s => s.id  == req.params.id
+    );            
+    if(!snippet){
+        return res.status(404).json({ message : "Snippet not found"});       //if the snippet is not found then we are returning a response 404 with error message i.e. snippet not found
+    }
+
+    const newComment = {
+        id: Date.now(),
+        text : req.body.text,
+    };
+
+    data.snippets.snippet.comment.push(newComment); // adding a new comment for that snippet
+   
+    writeDataDb(data); //now overwriting the complete data for db.json
+    res.json(newComment);
+
+});
+
 //Running the server
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
