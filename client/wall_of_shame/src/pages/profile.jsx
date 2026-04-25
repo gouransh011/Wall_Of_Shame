988322408ee2f8c20ce51746a79cf40cf8f1d941
Gouransh <gouranshgupta01@gmail.com>
2026-04-25 11:32:43 +0530

    feat: also displayed the comments done by the user

--- client/wall_of_shame/src/pages/profile.jsx
+++ client/wall_of_shame/src/pages/profile.jsx
@@ -2,18 +2,35 @@ import { useEffect, useState } from "react";
 
 function ProfilePage() {
   const [mySnippets, setMySnippets] = useState([]);
+  const [comments, setComments] = useState([]);
 
   let currentUser = localStorage.getItem("userId");
 
-  useEffect(() => {
+ useEffect(() => {
     fetch("http://localhost:5000/snippets")
       .then(res => res.json())
       .then(data => {
-        // filtering only user snippets
-        const userSnippets = data.filter(
-          s => s.userId === currentUser
-        );
-        setMySnippets(userSnippets);
+
+        // fetching our sinppets 
+        const mySnippets = data.filter(s => s.userId === currentUser);
+        setMySnippets(mySnippets);
+
+        // fetching our comments
+        let myComments = [];
+        data.forEach(s => {
+          if (s.comments) {
+            s.comments.forEach(c => { if (c.userId === currentUser) {
+                myComments.push({
+                  text: c.text,
+                  id: c.id,
+                  snippet: s.confession
+                })
+              }
+            });
+          }
+        });
+
+        setComments(myComments);
       });
   }, []);
 
@@ -21,14 +38,32 @@ function ProfilePage() {
     <div>
       <h1>My Profile</h1>
 
-      <h3>Your Snippets </h3>
+      <h3>Your Snippets</h3>
+    
        { mySnippets.map(s => (
           <div key={s.id} className="card">
             <div className="confession">{s.confession}</div>
+            <div className="snippet-date">
+              {new Date(s.id).toLocaleString()}
+            </div>
             <pre className="code">{s.code}</pre>
           </div>
-        ))}
-      
+        ))
+        }
+        
+
+      <h3>My Comments</h3>
+        
+       { comments.map(c => (
+          <div key={c.id} className="comment">
+            <div>{c.text}</div>
+
+            <div className="comment-date">
+              {new Date(c.id).toLocaleString()}
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 }
